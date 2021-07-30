da3305cfb6e2e09b3dfec55841c6836267a32cef
Soner Koçak <sonerkocak@gmail.com>
2021-07-30 07:50:39 +0300

    fixed Books.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import {Books} from "./component/Book";
+import Books from "./components/Books";
 
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                dxdg
+                <Books />
             </header>
         </div>
     );
